aebee9bc2cdee4acdf3dd85880615d0c75cd5ea3
codingdayo <145878788+codingdayo@users.noreply.github.com>
2025-02-04 00:54:03 +0100

    cors

---
+++ src/main/java/codingdayo/hng_stage1/cors/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package codingdayo.hng_stage1.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
